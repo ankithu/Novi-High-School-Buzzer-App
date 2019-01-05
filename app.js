a6f91e5b46a9bb4b899671659ca540b594d41e35
Ankith Udupa <ankith.udupa@gmail.com>
2019-01-04 20:13:46 -0500

    code dump

---
+++ app.js
@@ -0,0 +1,58 @@
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+var playerIDDict = {};
+var serverIDDict = {};
+var serverNum = 1000;
+var currentPlayerAssignment = 0;
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+const port=process.env.PORT || 3000
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+// // Starts the server.
+// server.listen(5000, '0.0.0.0',function() {
+//    console.log(server.address());
+//    console.log('Starting server on port 5000');
+// });
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+});
+
+var buzzerStates = {};
+io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    console.log(socket.id);
+   playerIDDict[socket.id] =  currentPlayerAssignment;
+    io.sockets.emit('assignment', currentPlayerAssignment);
+    currentPlayerAssignment++;
+    buzzerStates[playerIDDict[socket.id]] = false;
+  });
+  socket.on('buzzer', function(buzz){
+    playerId = buzz.split(':')[1];
+    serverId = buzz.split(':')[0];
+    console.log('has buzz from' + buzz);
+    console.log(buzz);
+    io.sockets.emit('buzzersStates', buzz);
+  });
+  socket.on('newServer', function(playerID){
+    serverIDDict[serverNum] = playerID;
+  });
+  socket.on('resetBuzzers', function(serverName){
+    console.log(serverName);
+    io.sockets.emit('resetBuzzers', serverName);
+  });
+
+});
---
+++ static/buzzer.js
@@ -0,0 +1,112 @@
+var socket = io();
+socket.on('message', function(data) {
+  console.log(data);
+});
+
+var buzzerHasBeenPressed = false;
+var buzzer = false;
+var isPlayer = true;
+var playerNum = -1;
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 32:
+      buzzer = true;
+      break;
+  }
+});
+socket.emit('new player');
+
+
+var button = document.getElementById("button");
+button.innerHTML = "Buzz";
+
+var serverButton = document.getElementById("createServer");
+
+// // 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+var message = document.getElementById("message");
+message.innerHTML = "BUZZ WHEN YOU ARE READY";
+
+
+body.appendChild(message);
+var socket = io.connect();
+var serverForm = document.getElementById("serverF");
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  if (!buzzerHasBeenPressed){
+      buzzer = serverForm.value + ":" + playerNum;
+      socket.emit('buzzer', buzzer);
+  }
+});
+
+
+
+serverButton.addEventListener ("click", function(){
+  if (isPlayer){
+    socket.emit('newServer', playerNum);
+    body.removeChild(button);
+    isPlayer = false;
+    message.innerHTML = "Buzzers are ready";
+    serverButton.innerHTML = "Reset Buzzers";
+  }
+  else{
+    socket.emit('resetBuzzers', serverForm.value);
+    body.style.background = "yellow";
+  }
+
+});
+
+socket.on('assignment', function(num){
+  if (playerNum < 0){
+    playerNum = num;
+  }
+});
+socket.on('resetBuzzers', function(name){
+  console.log(name);
+  console.log(serverForm.value);
+  if (name == serverForm.value){
+    buzzerHasBeenPressed = false;
+    body.style.background = "yellow";
+    if (isPlayer){
+      message.innerHTML = "BUZZ WHEN YOU ARE READY";
+    }
+    else{
+      message.innerHTML = "Buzzers are ready";
+    }
+  }
+  else{
+    console.log('reset was for someone else');
+  }
+});
+
+socket.on('buzzersStates', function(states){
+  console.log("has states");
+  console.log(states);
+  console.log(playerNum);
+  console.log(states.split(':')[1]);
+  if (serverForm.value == states.split(':')[0]){
+    if (isPlayer){
+      if(playerNum == states.split(':')[1]){
+        body.style.background = "green";
+        message.innerHTML = "Buzzed!";
+        buzzerHasBeenPressed = true;
+      }
+      else{
+        buzzerHasBeenPressed = true;
+        body.style.backgroundColor = "red";
+        message.innerHTML = "Someone else buzzed";
+      }
+    }
+    else{
+      message.innerHTML = states.split(':')[1] + " buzzed";
+      body.style.background = "green";
+    }
+
+    buzzerHasBeenPressed = true;
+
+  }
+
+});
